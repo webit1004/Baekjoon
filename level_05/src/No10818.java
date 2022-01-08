fef428354b7bcf8fe7dc4ec2302f5529dc99fbfd
jbyun <binifia@gmail.com>
2022-01-08 23:22:56 +0900

    commit_220108

---
+++ level_05/src/No10818.java
@@ -0,0 +1,32 @@
+package level_05;
+
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class No10818 {
+	public static void main(String[] args){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			int n = Integer.parseInt(br.readLine());
+			StringTokenizer st = 
+					new StringTokenizer(br.readLine(), " ");
+			
+			int index = 0;
+			int[] arr = new int[n];
+			while(st.hasMoreTokens()) {
+				arr[index] = Integer.parseInt(st.nextToken());
+				index++;
+			}
+			
+			Arrays.sort(arr);
+			System.out.println(arr[0] + " " + arr[n-1]);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+    }
+}
---
+++ level_05/src/No2577.java
@@ -0,0 +1,33 @@
+package level_05;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class No2577 {
+	public static void main(String[] args) {
+		
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			int[] cnt = new int[10]; // 0~9 개수 저장
+			int total;
+			
+			total = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+			
+			while(total != 0) {
+				cnt[total%10]++;
+				total/=10;
+			}
+            
+			for(int result : cnt) {
+				System.out.println(result);
+			}
+            
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
